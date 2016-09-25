aace9f5a66de12b06caa76b2db24ed60d67e91c9
Valentino Gonzalez <valentino.g.gonzalez@gmail.com>
2016-09-25 18:15:13 -0300

    Plantea variables para comparacion de tiempo de ejecucion solve vs solve_triangular

--- demo.py
+++ demo.py
@@ -13,8 +13,11 @@ A = np.array([[ 0, 1, 1, -3],
               [ 3, 1, 0,  0]])
 
 b = np.array([3, -2, 5, 1]).transpose()
+
+Tambien demostramos la eficiencia de solve_triangular vs. solve.
 """
 
+from __future__ import division
 import numpy as np
 
 
@@ -24,3 +27,19 @@ A = np.array([[0, 1, 1, -3],
               [3, 1, 0,  0]])
 
 b = np.array([3, -2, 5, 1])
+
+
+
+############################################################
+# Eficiencia de solve_triangular vs. solve
+############################################################
+np.random.seed(12389)
+A = np.random.randint(-10, 20, size=(100, 100))
+for i in range(100):
+    for j in range(i):
+        A[i, j] = 0
+
+for i in range(100):
+    A[i, i] = 1
+
+b = np.random.randint(30, 40, size=100)
